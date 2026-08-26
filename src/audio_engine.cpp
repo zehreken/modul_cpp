1be8660c9c174fa467d33a56e364327cfdbd9038
zehreken <zehreken@gmail.com>
2026-08-26 13:25:51 +0200

    Fix formatting

--- src/audio_engine.cpp
+++ src/audio_engine.cpp
@@ -7,8 +7,7 @@
 #include <cmath>
 #include <cstring>
 
-struct AudioEngine::Impl
-{
+struct AudioEngine::Impl {
     ma_context context;
     ma_device device;
     bool is_device_initialized{false};
@@ -17,55 +16,50 @@ struct AudioEngine::Impl
 
 AudioEngine::AudioEngine() : impl_(new Impl()) {}
 
-AudioEngine::~AudioEngine()
-{
+AudioEngine::~AudioEngine() {
     shutdown();
     delete impl_;
 };
 
-bool AudioEngine::init()
-{
-    if (ma_context_init(nullptr, 0, nullptr, &impl_->context) != MA_SUCCESS)
-    {
+bool AudioEngine::init() {
+    if (ma_context_init(nullptr, 0, nullptr, &impl_->context) != MA_SUCCESS) {
         return false;
     }
     impl_->is_context_initialized = true;
     return select_devices(-1, -1); // init with default device
 }
 
-void AudioEngine::shutdown()
-{
-    if (impl_->is_device_initialized)
-    {
+void AudioEngine::shutdown() {
+    if (impl_->is_device_initialized) {
         ma_device_uninit(&impl_->device);
         impl_->is_device_initialized = false;
     }
-    if (impl_->is_context_initialized)
-    {
+    if (impl_->is_context_initialized) {
         ma_context_uninit(&impl_->context);
         impl_->is_context_initialized = false;
     }
 }
 
-AudioDevices AudioEngine::get_audio_devices()
-{
+AudioDevices AudioEngine::get_audio_devices() {
     std::vector<AudioDeviceInfo> playback_devices;
-    ma_device_info *pPlaybackInfos;
-    ma_uint32 playbackCount;
+    ma_device_info* playback_infos;
+    ma_uint32 playback_count;
 
     std::vector<AudioDeviceInfo> capture_devices;
-    ma_device_info *pCaptureInfos;
-    ma_uint32 captureCount;
+    ma_device_info* capture_infos;
+    ma_uint32 capture_count;
 
-    if (ma_context_get_devices(&impl_->context, &pPlaybackInfos, &playbackCount, &pCaptureInfos, &captureCount) == MA_SUCCESS)
-    {
-        for (ma_uint32 i = 0; i < playbackCount; ++i)
-        {
-            playback_devices.push_back({pPlaybackInfos[i].name, static_cast<int>(i), pPlaybackInfos[i].isDefault != 0});
+    if (ma_context_get_devices(&impl_->context, &playback_infos, &playback_count,
+                               &capture_infos, &capture_count) == MA_SUCCESS) {
+        for (ma_uint32 i = 0; i < playback_count; ++i) {
+            playback_devices.push_back({playback_infos[i].name,
+                                        static_cast<int>(i),
+                                        playback_infos[i].isDefault != 0});
         }
-        for (ma_uint32 i = 0; i < captureCount; ++i)
-        {
-            capture_devices.push_back({pCaptureInfos[i].name, static_cast<int>(i), pCaptureInfos[i].isDefault != 0});
+        for (ma_uint32 i = 0; i < capture_count; ++i) {
+            capture_devices.push_back({capture_infos[i].name,
+                                       static_cast<int>(i),
+                                       capture_infos[i].isDefault != 0});
         }
     }
 
@@ -73,10 +67,9 @@ AudioDevices AudioEngine::get_audio_devices()
     return devices;
 }
 
-bool AudioEngine::select_devices(int playback_device_index, int capture_device_index)
-{
-    if (impl_->is_device_initialized)
-    {
+bool AudioEngine::select_devices(int playback_device_index,
+                                 int capture_device_index) {
+    if (impl_->is_device_initialized) {
         ma_device_uninit(&impl_->device);
         impl_->is_device_initialized = false;
     }
@@ -91,23 +84,24 @@ bool AudioEngine::select_devices(int playback_device_index, int capture_device_i
     config.dataCallback = AudioEngine::c_audio_callback;
     config.pUserData = this;
 
-    ma_device_info *pPlaybackInfos;
-    ma_uint32 playbackCount;
-    ma_device_info *pCaptureInfos;
-    ma_uint32 captureCount;
-    ma_context_get_devices(&impl_->context, &pPlaybackInfos, &playbackCount, &pCaptureInfos, &captureCount);
-    if (playback_device_index >= 0)
-    {
-        config.playback.pDeviceID = &pPlaybackInfos[playback_device_index].id;
+    ma_device_info* playback_infos;
+    ma_uint32 playback_count;
+    ma_device_info* capture_infos;
+    ma_uint32 capture_count;
+    if (ma_context_get_devices(&impl_->context, &playback_infos, &playback_count,
+                               &capture_infos, &capture_count) != MA_SUCCESS) {
+        return false;
+    }
+    if (playback_device_index >= 0 && playback_device_index < playback_count) {
+        config.playback.pDeviceID = &playback_infos[playback_device_index].id;
     }
 
-    if (capture_device_index >= 0)
-    {
-        config.capture.pDeviceID = &pCaptureInfos[capture_device_index].id;
+    if (capture_device_index >= 0 && capture_device_index < capture_count) {
+        config.capture.pDeviceID = &capture_infos[capture_device_index].id;
     }
 
-    if (ma_device_init(&impl_->context, &config, &impl_->device) != MA_SUCCESS)
-    {
+    if (ma_device_init(&impl_->context, &config, &impl_->device) !=
+        MA_SUCCESS) {
         return false;
     }
 
@@ -115,20 +109,20 @@ bool AudioEngine::select_devices(int playback_device_index, int capture_device_i
     return ma_device_start(&impl_->device) == MA_SUCCESS;
 }
 
-void AudioEngine::c_audio_callback(ma_device *pDevice, void *pOutput, const void *pInput, ma_uint32 frameCount)
-{
-    auto *engine = static_cast<AudioEngine *>(pDevice->pUserData);
-    engine->process_audio(static_cast<float *>(pOutput), static_cast<const float *>(pInput), frameCount);
+void AudioEngine::c_audio_callback(ma_device* pDevice, void* pOutput,
+                                   const void* pInput, ma_uint32 frameCount) {
+    auto* engine = static_cast<AudioEngine*>(pDevice->pUserData);
+    engine->process_audio(static_cast<float*>(pOutput),
+                          static_cast<const float*>(pInput), frameCount);
 }
 
-void AudioEngine::process_audio(float *output, const float *input, unsigned int frameCount)
-{
+void AudioEngine::process_audio(float* output, const float* input,
+                                unsigned int frameCount) {
     float freq = frequency_.load(std::memory_order_relaxed);
     float vol = volume_.load(std::memory_order_relaxed);
     float phase_incr = (TWO_PI * freq) / SAMPLE_RATE;
 
-    for (unsigned int i = 0; i < frameCount; ++i)
-    {
+    for (unsigned int i = 0; i < frameCount; ++i) {
         float sample = std::sin(phase_) * vol;
 
         float in_left = input ? *input++ : 0.0f;
@@ -149,8 +143,7 @@ void AudioEngine::process_audio(float *output, const float *input, unsigned int 
     }
 }
 
-void AudioEngine::copy_scope_buffer(float *out_target, size_t count)
-{
+void AudioEngine::copy_scope_buffer(float* out_target, size_t count) {
     size_t copy_size = std::min(count, SCOPE_SIZE);
     std::memcpy(out_target, scope_buffer_, copy_size * sizeof(float));
 }
